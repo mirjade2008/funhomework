f9d447bcd9123ffbd7f8d78e5b1b5cc411132646
mirjade2008 <mirjade2008@yahoo.com>
2021-11-04 18:30:47 -0700

    funhomework

---
+++ app.js
@@ -0,0 +1,28 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+console.log(a + b);
+console.log(d + e);
+
+console.log(c - b);
+console.log(h - g);
+
+console.log(e * g);
+console.log(h * i);
+
+console.log(j / b);
+console.log(h / d);
+
+console.log(e++);
+console.log(e);
+
+console.log(f--);
+console.log(f);
